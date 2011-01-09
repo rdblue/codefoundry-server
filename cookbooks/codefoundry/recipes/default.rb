b49cecc50630af60ae874ee780e6390928391af1
Ryan Blue <rdblue@gmail.com>
2011-01-09 17:11:04 +0100

    Some progress on the CF default recipe.
    It now requries apache2, subversion, and mod_dav_svn.  Directories are created
    for svn and git repositories, and the vhost setup blocks are partially done but
    commented out.

--- cookbooks/codefoundry/recipes/default.rb
+++ cookbooks/codefoundry/recipes/default.rb
@@ -16,5 +16,45 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# nothing for now...
-package "vim"
+include_recipe 'apache2'
+
+# set up SVN hosting through apache and mod_dav_svn
+include_recipe 'apache2::mod_dav_svn'
+include_recipe 'subversion::client'
+
+# storage
+directory "#{node[:codefoundry][:repo_dir]}/svn" do
+  recursive true
+  owner node[:apache][:user]
+  group node[:apache][:user]
+  mode "0755"
+end
+
+# the apache vhost
+#web_app "subversion" do
+#  template "svn-vhost.conf.erb"
+#  server_name "svn-host"
+#end
+
+# set up git hosting storage
+directory "#{node[:codefoundry][:repo_dir]}/git" do
+  recursive true
+  owner node[:apache][:user]
+  group node[:apache][:user]
+  mode "0755"
+end
+
+# set up the CodeFoundry application
+
+# clone the repository
+# update the repository
+# check out the correct tag
+# create db.yml
+# create settings.yml
+
+# create the apache vhost
+#web_app "codefoundry" do
+#  template "cf-vhost.conf.erb"
+#  server_name "codefoundry"
+#end
+
